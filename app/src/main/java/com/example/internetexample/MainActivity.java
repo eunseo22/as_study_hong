0f8245fb4f2cc407cd9e9df41eab7de059774d0f
eunseo <es@jbnu.ac.k>
2020-09-19 18:16:40 +0900

    6th hong

--- app/src/main/java/com/example/internetexample/MainActivity.java
+++ app/src/main/java/com/example/internetexample/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_move;
     private EditText et_test;
     private String str;
+    private ImageView iv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         et_test = findViewById(R.id.et_test);
-        str = et_test.getText().toString();
 
         btn_move = findViewById(R.id.btn_move);
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                str = et_test.getText().toString();
                 Intent intent = new Intent(MainActivity.this , SubActivity.class );
-                Intent.putExtra("str", str);
+                intent.putExtra("str", str);
                 startActivity(intent);
+            }
+        });
+
+        iv_test = findViewById(R.id.iv_test);
+        iv_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "launcher", Toast.LENGTH_SHORT).show();
             }
         });
 
